9d929ea5ab8d391cdd40ad4b0186d2c0b803a4b7
Luiz <lvj253@gmail.com>
2022-11-14 04:10:35 -0300

    config para deploy

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,4 +3,12 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    localhost: {
+      url: "http://localhost:8545"
+    },
+    localhost2: {
+      url: "http://localhost:8546"
+    }
+  }
 };
